067f4983ae767538f575cd49fb495d5edb76c198
AungPyaeSone <aungpyaesoneice@gmail.com>
2019-12-05 21:04:21 +0630

    time picker

--- app/src/main/java/com/example/pickerfordate/MainActivity.java
+++ app/src/main/java/com/example/pickerfordate/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.pickerfordate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnDate = findViewById(R.id.btnDate);
+
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+    public void processDatePicker(int year,int month,int day){
+        String year_string = Integer.toString(year);
+        String month_string = Integer.toString(month);
+        String day_string = Integer.toString(day);
+        String dateMessage = month_string+ "/" + day_string + "/" + year_string;
+        Toast.makeText(this, "Date "+ dateMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showTimePicker(View view) {
+        DialogFragment timePickerFragment = new TimePickerFragment();
+        timePickerFragment.show(getSupportFragmentManager(),"timePicker");
+
+    }
+
+    public void processTimePicker(int hour,int minute){
+        String hour_string = Integer.toString(hour);
+        String minute_string = Integer.toString(minute);
+        String dataMessage = hour_string + ":" + minute_string;
+        Toast.makeText(this, "Time "+ dataMessage, Toast.LENGTH_SHORT).show();
+
     }
 }
---
+++ app/src/main/java/com/example/pickerfordate/TimePickerFragment.java
@@ -0,0 +1,42 @@
+package com.example.pickerfordate;
+
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        return new TimePickerDialog(getActivity(),this,hour,minute,false);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        MainActivity mainActivity = (MainActivity) getActivity();
+        int hour = hourOfDay - 12;
+        mainActivity.processTimePicker(hour,minute);
+    }
+}
